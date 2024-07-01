0efe5bf4b7e397b99864a426c7a6598373a506dc
horizenight <kshitijroodkee1@gmail.com>
2024-07-01 12:42:14 +0530

    [dev]: implement /loadIds

--- Backend/App/ids/ids.py
+++ Backend/App/ids/ids.py
@@ -1,21 +1,46 @@
-from fastapi import APIRouter, HTTPException
-from typing import List
-from pydantic import BaseModel
+import os
+import tempfile
+import uuid
+from fastapi import APIRouter, HTTPException, UploadFile, File
+import ifctester
+import ifctester.reporter
+import ifcopenshell
 
-router = APIRouter()
+router = APIRouter(prefix="/api/ids")
 
-class IdItem(BaseModel):
-    id: int
 
-ids_db = []
+def transform(data):
+    info = data.get('info', {})
+    specifications = data.get('specifications', {})
 
-@router.post("/saveIds")
-async def save_ids(ids: List[IdItem]):
-    ids_db.extend(ids)
-    return {"message": "IDs saved successfully"}
+    transformed_data = {
+        "info": {
+            "title": info.get('title'),
+            "identifier": str(uuid.uuid4()),
+            "copyright": info.get('copyright'),
+            "version": info.get('version'),
+            "description": info.get('description'),
+            "author": info.get('author'),
+            "date": info.get('date'),
+            "purpose": info.get('purpose'),
+            "milestone": info.get('milestone'),
+        },
+        "specifications": specifications.get('specification', [])
+    }
+    return transformed_data
+    
+@router.post("/loadIds")
+async def audit(ids_file: UploadFile = File(...)):
+    if not ids_file.filename.endswith(".ids"):
+        raise HTTPException(status_code=400, detail="Invalid file extension. Only .ids files are allowed.")
 
-@router.get("/loadIds")
-async def load_ids():
-    if not ids_db:
-        raise HTTPException(status_code=404, detail="No IDs found")
-    return ids_db
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".ids") as tmp_file:
+        try:
+            tmp_file.write(await ids_file.read())
+            tmp_file.flush()
+            ids_data = ifctester.open(tmp_file.name)
+            return {"data": transform(ids_data.asdict())}
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error processing file: {str(e)}")
+        finally:
+            os.remove(tmp_file.name)
