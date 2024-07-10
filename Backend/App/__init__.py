11ca1ee33e3bd12b1e72d21afd9d85c937f7ddca
horizenight <kshitijroodkee1@gmail.com>
2024-07-10 12:39:06 +0530

    [dev]: resolved CORS

--- Backend/App/__init__.py
+++ Backend/App/__init__.py
@@ -1,11 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .ids import ids
 from datetime import datetime
 
 app = FastAPI()
-
-# Include the router from the ids module
 app.include_router(ids.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 # Create a route to check if the API is live and provide status information
 @app.get("/")
@@ -14,5 +21,5 @@ async def root():
         "status": "API is live",
         "timestamp": datetime.utcnow(),
         "version": "1.0.0", 
-        "idsSchemaVersion":"//TODO"
+        "idsSchemaVersion": "//TODO"
     }
