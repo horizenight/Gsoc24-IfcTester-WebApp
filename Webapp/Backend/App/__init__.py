aa77b4535c4fe2a9a04cf02b20e35eca42faff23
horizenight <kshitijroodkee1@gmail.com>
2024-09-14 22:49:43 +0530

    [dev]: Backend serve

--- Webapp/Backend/App/__init__.py
+++ Webapp/Backend/App/__init__.py
@@ -1,11 +1,17 @@
+import os
+from datetime import datetime
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+
 from .ids import ids
-from datetime import datetime
 
 app = FastAPI()
 app.include_router(ids.router)
 
+# allows all origins 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  
@@ -14,8 +20,20 @@ app.add_middleware(
     allow_headers=["*"],  
 )
 
+app.mount("/static", StaticFiles(directory=os.path.join(os.path.dirname(__file__), "../../Frontend")), name="static")
+
+
+#API Routes: 
+
+# Create a route to serve static files
+@app.get("/", response_class=HTMLResponse)
+async def serve_html():
+    with open(os.path.join(os.path.dirname(__file__), "../../Frontend/index.html")) as f:
+        return HTMLResponse(content=f.read(), status_code=200)
+
+
 # Create a route to check if the API is live and provide status information
-@app.get("/")
+@app.get("/api")
 async def root():
     return {
         "status": "API is live",
